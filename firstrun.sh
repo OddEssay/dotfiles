fbd04e427e9adabe84722c839cf217480039b580
Paul Bennett-Freeman <clicknmix@gmail.com>
2018-10-19 11:15:27 +0100

    Output some details of what is happening on firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,3 +1,7 @@
+echo "\n\nInstalling pip...\n\n"
 sudo easy_install pip
+echo "\n\nInstalling Ansible...\n\n"
 sudo pip install ansible
+echo "\n\nInstalling Homebrew...\n\n"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "\n\nNow ready to run `ansible-playbook install-playbook.yml`"
