9e2f93b9ede3b8011ec06b85cf72a61cfdf1292d
Paul Bennett-Freeman <clicknmix@gmail.com>
2024-11-09 16:22:03 +0000

    Fix warnings with deprecated which-key format

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -190,19 +190,19 @@ require("lazy").setup({
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>c", name = "[C]ode" },
+				{ "<leader>d", name = "[D]ocument" },
+				{ "<leader>r", name = "[R]ename" },
+				{ "<leader>s", name = "[S]earch" },
+				{ "<leader>w", name = "[W]orkspace" },
+				{ "<leader>t", name = "[T]oggle" },
+				{ "<leader>h", name = "Git [H]unk" },
 			})
 			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
+			require("which-key").add({
+				{ "<leader>h", "Git [H]unk", mode = "v" },
+			})
 		end,
 	},
 
