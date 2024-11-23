b674110330b44ab8bbc6731e518f1fe8caa663b9
Paul Bennett-Freeman <clicknmix@gmail.com>
2024-11-23 11:42:39 +0000

    Allow Wezterm and Neovim to play nicely together

---
+++ config/nvim/lua/plugins/wezterm.lua
@@ -0,0 +1,29 @@
+return {
+	"letieu/wezterm-move.nvim",
+	keys = { -- Lazy loading, don't need call setup() function
+		{
+			"<C-h>",
+			function()
+				require("wezterm-move").move("h")
+			end,
+		},
+		{
+			"<C-j>",
+			function()
+				require("wezterm-move").move("j")
+			end,
+		},
+		{
+			"<C-k>",
+			function()
+				require("wezterm-move").move("k")
+			end,
+		},
+		{
+			"<C-l>",
+			function()
+				require("wezterm-move").move("l")
+			end,
+		},
+	},
+}
