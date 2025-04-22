22583875e847b42f6cb3a98cc0f87786419a283e
Paul Bennett-Freeman <clicknmix@gmail.com>
2025-04-22 21:55:18 +0100

    Automagically create directories if they don't exist when saving a file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -64,6 +64,18 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 25
 
+-- Auto-create directories when saving files
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = "*",
+	callback = function(event)
+		if event.match:match("^%w+://") then
+			return
+		end
+		local file = vim.loop.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
